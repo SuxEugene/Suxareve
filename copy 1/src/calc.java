d0ae6d9bab2b27d9937c38c56e8693af9088a00c
SuxEugene <SuxEugene@192.168.0.3>
2016-10-26 09:39:54 +0300

    Added to git hub.

---
+++ copy 1/src/calc.java
@@ -0,0 +1,15 @@
+
+public class calc {
+	static String a;
+	
+	public static String count(String d1, int m){
+		if(!d1.equals("0")){
+			a = d1;
+			a += Integer.toString(m);
+		}else{
+			a = d1;
+			a = Integer.toString(m);
+		}
+		return a;
+	}
+}
---
+++ copy 1/src/dlc.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+
+public class dlc {
+	public static void main(String[] args) {
+		Reader r = new Reader("Калькулятор");
+	r.setVisible(true);
+	r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	r.setSize(200, 250);
+	r.setResizable(false);
+	r.setLocationRelativeTo(null);
+	}
+}
